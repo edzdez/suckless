0a763668539cb8ef25f465468cad187cba5f29bd
Ethan Zhang <ezsland05@gmail.com>
2021-10-06 18:48:48 -0700

    now the get vol script tells you if the sink is muted

--- dwmblocks/scripts/getvol.sh
+++ dwmblocks/scripts/getvol.sh
@@ -3,7 +3,9 @@
 SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
 NOW=$( pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
 
-if [ "$NOW" -ge 60 ]; then
+if [ "$(pamixer --get-mute)" = "true" ]; then
+    echo "ﱝ $NOW% "	
+elif [ "$NOW" -ge 60 ]; then
    echo " $NOW% "
 elif [ "$NOW" -ge 40 ]; then
    echo " $NOW% "
