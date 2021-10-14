f27fc4f84c14814834920ceb3a36a8e130916972
Ethan Zhang <ezsland05@gmail.com>
2021-10-13 22:46:31 -0700

    changed kernel icon and added a config file for a bottom bar

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,7 +1,8 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"^c#2e3440^^b#bf616a^  ", "/home/edzdez/suckless/dwmblocks/scripts/getkern.sh", 3600, 0},
+    /* {"^c#2e3440^^b#bf616a^  ", "/home/edzdez/suckless/dwmblocks/scripts/getkern.sh", 3600, 0}, */
+    {"^c#2e3440^^b#bf616a^  ", "/home/edzdez/suckless/dwmblocks/scripts/getkern.sh", 3600, 0},
     {"^c#2e3440^^b#d08770^  ", "/home/edzdez/suckless/dwmblocks/scripts/cpu.sh", 10, 0},
     {"^c#2e3440^^b#ebcb8b^  ", "/home/edzdez/suckless/dwmblocks/scripts/mem.sh", 10, 0},
     {"^c#2e3440^^b#a3be8c^ ", "/home/edzdez/suckless/dwmblocks/scripts/getbl.sh", 5, 1},
